8f6e79fc468241374831054c27b3d4dc59fe2b24
Terence Fox <nd85573@dow.com>
2022-06-17 19:33:56 -0400

    Photography page

--- src/pages/photography.js
+++ src/pages/photography.js
@@ -1,9 +1,48 @@
 import * as React from "react";
 import Layout from "../components/layout";
+import { graphql } from "gatsby";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import Masonry from "react-masonry-component";
 
-// markup
-const IndexPage = ({ location }) => {
-  return <Layout location={location}>Hi. I'm the photography page</Layout>;
+const IndexPage = ({ location, data }) => {
+  const images = data.allImageSharp.nodes
+    .filter((node) => node.parent.relativeDirectory === "photography")
+    .map((node) => {
+      return getImage(node);
+    });
+  return (
+    <Layout location={location}>
+      <p>
+        Interested in buying prints?{" "}
+        <a href="mailto:terencecfox@gmail.com">Shoot me an email.</a>
+      </p>
+      <div className="photo-grid">
+        {images.map((image, i) => {
+          console.log(image);
+          return (
+            <div className="photo-wrapper" id={`photo${i + 1}`}>
+              <GatsbyImage image={image} key={i} alt="some bullshit" />
+            </div>
+          );
+        })}
+      </div>
+    </Layout>
+  );
 };
 
 export default IndexPage;
+
+export const photosQuery = graphql`
+  query {
+    allImageSharp {
+      nodes {
+        gatsbyImageData(formats: WEBP, placeholder: BLURRED, width: 1600)
+        parent {
+          ... on File {
+            relativeDirectory
+          }
+        }
+      }
+    }
+  }
+`;
